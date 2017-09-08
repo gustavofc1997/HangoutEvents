56b567bb2eee3dc7ca47d07455daf9083b20a81d
GustavoDev97 <gustavo.forero1997@gmail.com>
2017-09-07 22:33:24 -0500

    add splash
    base activities

---
+++ Hangout/app/src/main/java/events/hangout/com/hangout/ui/activities/PreferencesActivity.java
@@ -0,0 +1,10 @@
+package events.hangout.com.hangout.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Gustavo on 7/09/2017.
+ */
+
+public class PreferencesActivity extends AppCompatActivity {
+}
---
+++ Hangout/app/src/main/java/events/hangout/com/hangout/ui/activities/SplashActivity.java
@@ -0,0 +1,16 @@
+package events.hangout.com.hangout.ui.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+        // close splash activity
+        finish();
+    }
+}
