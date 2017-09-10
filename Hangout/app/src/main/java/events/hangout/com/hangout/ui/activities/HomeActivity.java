28d11b290aef47fcdcde9721fbe12ed7ec911e4b
GustavoDev97 <gustavo.forero1997@gmail.com>
2017-09-10 17:46:14 -0500

    add firebase

--- Hangout/app/src/main/java/events/hangout/com/hangout/ui/activities/HomeActivity.java
+++ Hangout/app/src/main/java/events/hangout/com/hangout/ui/activities/HomeActivity.java
@@ -1,10 +1,22 @@
 package events.hangout.com.hangout.ui.activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import events.hangout.com.hangout.R;
 
 /**
  * Created by Gustavo on 7/09/2017.
  */
 
 public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+    }
 }
